abbdaad69b1ad7725d017f3fd6668c8715e16480
clhasse <clhasse@users.noreply.github.com>
2015-07-25 08:49:04 -0700

    Commit for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions first create a matrix and store the inverse of the matrix through 
+## makeCacheMatrix and then, when the inverse of a matrix is asked for via cacheSolve, 
+## the function (cacheSolve) looks to see if that inverse is stored and the stored  
+## version is returned if it exists and if not, the inverse is calculated and returned.
 
-## Write a short comment describing this function
+## Specifically, makeCacheMatrix sets the value of the matrix, gets the value of the matrix, 
+## sets the value of the inverse of the matrix, gets the value of the inverse matrix, and 
+## caches the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(solve) 
+                m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) 
 }
 
-
-## Write a short comment describing this function
+## cacheSolve identifies whether the inverse of the same matrix has already been computed 
+## and if so, returns the applicable message and then the inverse from the cache; otherwise, 
+## it computes the inverse of the matrix and returns that.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInverse(m)
+        m
+} 
+## Please note (in the spirit of the honor code) that the assignment instructions 
+## provided the majority of this code.
